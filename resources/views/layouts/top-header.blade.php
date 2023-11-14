1f27fb5544448f91cdfcd91e53ab30f0beba2440
munthir100 <munthiromer100@gmail.com>
2023-11-14 16:25:53 +0200

    edit on localization

--- resources/views/layouts/top-header.blade.php
+++ resources/views/layouts/top-header.blade.php
@@ -20,13 +20,13 @@
                     </ul>
                     @else
                     <button class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" type="button" aria-haspopup="true" aria-expanded="true">
-                        <img src="/vendor/core/core/base/images/flags/sa.svg" title="english" width="16" alt="english">
+                        <img src="/vendor/core/core/base/images/flags/us.svg" title="english" width="16" alt="english">
                         English
                     </button>
                     <ul class="dropdown-menu language_bar_chooser ">
                         <li>
                             <a>
-                                <img src="/vendor/core/core/base/images/flags/us.svg" title="arabic" width="16" alt="arabic">
+                                <img src="/vendor/core/core/base/images/flags/sa.svg" title="arabic" width="16" alt="arabic">
                                 <span onclick="document.getElementById('arForm').submit()">
                                     العربية
                                 </span>
