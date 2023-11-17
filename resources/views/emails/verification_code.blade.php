3c454c2122337fe5643441ff6a0e127854e4790b
munthir100 <munthiromer100@gmail.com>
2023-11-17 21:47:40 +0200

    edit in content of verification email code

--- resources/views/emails/verification_code.blade.php
+++ resources/views/emails/verification_code.blade.php
@@ -13,15 +13,15 @@
     <div class="container">
         <h2>Email Verification Code</h2>
         <p>Dear User,</p>
-        <p>You requested an email verification code. Your email verification code is:</p>
-        <p class="verification-code">{{ $verificationCode }}</p>
-        <p>Please use this code to verify your email address.</p>
-        <p>If you did not request this verification, you can safely ignore this email.</p>
+        <p>Thank you for choosing Aqaarat Platform. You recently requested an email verification code. Your unique verification code is:</p>
+        <p class="verification-code" style="font-size: 24px; font-weight: bold; color: #28a745;">{{ $verificationCode }}</p>
+        <p>Please use this code to complete the email verification process and secure your account.</p>
+        <p>If you did not initiate this verification, please ignore this email or contact our support team.</p>
         <div class="footer">
             <p>Best Regards,<br>Aqaarat Platform</p>
             <p>
-                <a href="{{ route('about-us') }}" target="_blank">About Us</a> |
-                <a href="{{ route('contactForm') }}" target="_blank">Contact</a>
+                <a href="{{ route('about-us') }}" style="color: #007bff; text-decoration: none;" target="_blank">About Us</a> |
+                <a href="{{ route('contactForm') }}" style="color: #007bff; text-decoration: none;" target="_blank">Contact</a>
             </p>
         </div>
     </div>
