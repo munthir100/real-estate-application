adc7384736821d9a91c07aac3a9a4b9e2fb925f9
munthir100 <munthiromer100@gmail.com>
2023-11-17 08:27:52 +0200

    install mailgun

--- config/mail.php
+++ config/mail.php
@@ -46,6 +46,12 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
+        'mailgun' => [
+            'transport' => 'mailgun',
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
