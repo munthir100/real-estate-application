e6d7567bdc8080f36daa3470caf585b769acdcaf
munthir100 <munthiromer100@gmail.com>
2023-11-17 20:24:08 +0200

    edit on content of email verification form

--- resources/views/emails/verification_code.blade.php
+++ resources/views/emails/verification_code.blade.php
@@ -5,53 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-
-        .container {
-            max-width: 600px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #ffffff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-            text-align: center;
-        }
-
-        h2 {
-            color: #007bff;
-            font-size: 28px;
-            margin-bottom: 10px;
-        }
-
-        p {
-            color: #333;
-            font-size: 16px;
-            line-height: 1.5;
-            margin-bottom: 15px;
-        }
-
-        .verification-code {
-            font-size: 32px;
-            font-weight: bold;
-            color: #28a745;
-            margin-bottom: 20px;
-        }
-
-        .footer {
-            margin-top: 20px;
-            color: #888;
-            font-size: 14px;
-        }
-
-        .footer a {
-            color: #007bff;
-            text-decoration: none;
-        }
+        /* Your styles here */
     </style>
 </head>
 
@@ -59,7 +13,7 @@
     <div class="container">
         <h2>Email Verification Code</h2>
         <p>Dear User,</p>
-        <p>You are request email verification code , your email verification code is:</p>
+        <p>You requested an email verification code. Your email verification code is:</p>
         <p class="verification-code">{{ $verificationCode }}</p>
         <p>Please use this code to verify your email address.</p>
         <p>If you did not request this verification, you can safely ignore this email.</p>
