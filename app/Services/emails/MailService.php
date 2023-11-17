bb4ccb82508076df2622bfce0f7fceda2cb9b6db
munthir100 <munthiromer100@gmail.com>
2023-11-17 20:30:34 +0200

    send email contact form new laravel syntax

--- app/Services/emails/MailService.php
+++ app/Services/emails/MailService.php
@@ -2,21 +2,25 @@
 
 namespace App\Services\emails;
 
+use App\Mail\ContactFormMail;
+use Illuminate\Support\Facades\Mail;
+
 class MailService
 {
     public function sendContactMessage(array $data)
     {
-        $message = view('emails.contact', compact('data'))->render();
-        $to = 'munthiromer100@gmail.com';
-        $subject = $data['subject'];
-        $fromEmail = $data['email'];
+        $mailSent = Mail::to('munthiromer100@gmail.com')->send(new ContactFormMail($data));
+        // $message = view('emails.contact', compact('data'))->render();
+        // $to = 'munthiromer100@gmail.com';
+        // $subject = $data['subject'];
+        // $fromEmail = $data['email'];
 
-        $headers = [
-            'Content-type: text/html; charset=UTF-8',
-            'From: ' . $fromEmail,
-        ];
+        // $headers = [
+        //     'Content-type: text/html; charset=UTF-8',
+        //     'From: ' . $fromEmail,
+        // ];
 
-        $mailSent = mail($to, $subject, $message, implode("\r\n", $headers));
+        // $mailSent = mail($to, $subject, $message, implode("\r\n", $headers));
 
         return $mailSent;
     }
